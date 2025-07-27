11c303dd5bfa2004b1111554c58bf2641f0d3a9c
REGI RIDHO <regiyanuar123@gmail.com>
2025-07-28 03:23:03 +0700

    footer sudah bener, kurang isi about

--- application/modules/my_blog/views/v_my_blog.php
+++ application/modules/my_blog/views/v_my_blog.php
@@ -1,25 +1,25 @@
 <div class="container">
 	<div class="main">
-    <div class="row justify-content-center">
-        <?php foreach ($dashboard_blog as $dsh) : ?>
-            <div class="col-md-8 col-10 mb-4">
-                <div class="d-flex flex-column flex-md-row border rounded p-3 bg-white shadow-sm">
-                    
-                    <!-- Gambar -->
-                    <div class="mb-3 mb-md-0 me-md-4 text-center">
-                        <img src="<?= base_url('assets/uploads/' . $dsh->gambar) ?>" 
-                             width="300" height="400" 
-                             class="rounded" alt="Gambar Blog">
-                    </div>
+        <div class="row justify-content-center">
+            <?php foreach ($dashboard_blog as $dsh) : ?>
+                <div class="col-md-8 col-10">
+                    <div class="d-flex flex-column flex-md-row border rounded p-3 bg-white shadow-sm">
+                        
+                        <!-- Gambar -->
+                        <div class="mb-3 mb-md-0 me-md-4 text-center">
+                            <img src="<?= base_url('assets/uploads/' . $dsh->gambar) ?>" 
+                                 width="300" height="400" 
+                                 class="rounded" alt="Gambar Blog">
+                        </div>
 
-                    <!-- Deskripsi -->
-                    <div class="">
-                        <p class="mb-0"><?= $dsh->about_blog ?></p>
+                        <!-- Deskripsi -->
+                        <div class="">
+                            <p class="mb-0"><?= $dsh->about_blog ?></p>
+                        </div>
+                        
                     </div>
-                    
                 </div>
+            <?php endforeach; ?>
             </div>
-        <?php endforeach; ?>
-        </div>
     </div>
 </div>
--- application/views/template_user/_partials/footer.php
+++ application/views/template_user/_partials/footer.php
@@ -1,29 +1,30 @@
-<footer class="footer  ">
-	<div class="row bg-dark bg-gradient text-white">
-	  <div class="col-md-6">
-	    <ul class="p-5 list-unstyled">
-	      <li>Contact</li>
-	      <li>oasdoasdasd</li>
-	      <li>Email</li>
-	      <li>regiblabla@gmail.com</li>
-	    </ul>
-	  </div>
-	  <div class="col-md-6">
-	    <ul class="p-5 list-unstyled">
-	      <li>Contact</li>
-	      <li>
-	        <div class="d-flex gap-3 mt-2">
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<footer class="main-footer text-center py-3 bg-dark text-white">
+    <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <div class="footer-left">
+            <span>&copy; 2025 <span class="bullet">•</span> Regi Yanuar</span>
+        </div>
+        <div class="footer-right mt-2 mt-md-0">
+        	<span>Contact</span>
+            <div class="d-flex gap-3 mt-2">
 	          <i class="fa-brands fa-instagram fa-2x"></i>
 	          <i class="fa-brands fa-square-facebook fa-2x"></i>
 	          <i class="fa-brands fa-twitter fa-2x"></i>
 	        </div>
-	      </li>
-	    </ul>
-	  </div>
-	</div>
-
+        </div>
+    </div>
 </footer>
 
+</div> <!-- Closing main container -->
+</div> <!-- Closing wrapper -->
 
-</html>
+<?php $this->load->view('template_user/_partials/js'); ?>
+
 </body>
+</html>
+
+
+
+
